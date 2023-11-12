470ecb5df483b5c284d39114e270b9bc0969298f
Ryoga-exe <git@ryoga.dev>
2023-11-13 02:06:13 +0900

    feat: add Common.hpp and Version.hpp

--- XLAIR/Main.cpp
+++ XLAIR/Main.cpp
@@ -1,8 +1,6 @@
-﻿#include <Siv3D.hpp> // Siv3D v0.6.12
+﻿#include "Common.hpp"
 
 void Main() {
-
     while (System::Update()) {
-
     }
 }
---
+++ XLAIR/src/Common.hpp
@@ -0,0 +1,3 @@
+﻿#pragma once
+#include <Siv3D.hpp> // Siv3D v0.6.12
+#include "Consts/Version.hpp"
